0535e27e983b91b10ee8bc31f77625945a339b2a
Indian1999 <haszab99@gmail.com>
2025-03-22 14:00:08 +0100

    tokenize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from keras.models import load_model
 import nltk
 from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
 import pandas as pd
 import numpy as np
 
@@ -16,3 +18,18 @@ def create_text_all():
     for review in data["review"]:
       text_all += review + " "
     return text_all
+
+def create_train_test_data():
+    x = list(data["review"])
+    y = list(data["division"])
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    return (x_train, x_test, y_train, y_test) 
+
+def tokenize_text(x):
+    text_all = create_text_all()
+    text_all = np.array(text_all.split(" "))
+    unique_words = np.unique(text_all)
+    tokenizer = Tokenizer(num_words = len(unique_words))
+
+x_train, x_test, y_train, y_test = create_train_test_data()
+
